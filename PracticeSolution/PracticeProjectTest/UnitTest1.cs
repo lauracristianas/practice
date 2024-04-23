066cd11b4c53723c7ecc1fda7e878580ea02dfa3
lauracristianas <45941279+lauracristianas@users.noreply.github.com>
2024-04-23 20:27:55 +0200

    Refactored all Tests into one Theory and added more test cases

--- PracticeSolution/PracticeProjectTest/UnitTest1.cs
+++ PracticeSolution/PracticeProjectTest/UnitTest1.cs
@@ -4,24 +4,22 @@ namespace PracticeProjectTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
-            // Arrange
-            int number1 = 5;
-            int number2 = 2;
-            int expectedVal = 10;
-
-            // Act
-            int actualVal = PracticeProject.Exercise1Class.Multiply(number1, number2);
-            
-            // Assert
-            Assert.Equal(expectedVal, actualVal);
-        }
-
         [Theory]
+        // testing normal numbers
+        [InlineData(5, 2, 10)]
+        [InlineData(2, 5, 10)]
+        [InlineData(5, 1, 5)]
+        [InlineData(1, 5, 5)]
+        // testing zeros
+        [InlineData(0, 6, 0)]
         [InlineData(6, 0, 0)]
-        public void Test2(int number1, int number2, int expectedVal)
+        [InlineData(0, 0, 0)]
+        // testing negatives
+        [InlineData(5, -2, -10)]
+        [InlineData(-2, 5, -10)]
+        [InlineData(-2, -2, 4)]
+        [InlineData(-9, 2, -18)]
+        public void Test(int number1, int number2, int expectedVal)
         {
             // Arrange
             // Act
