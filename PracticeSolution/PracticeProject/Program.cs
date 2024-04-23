0472ad485275e2b1a0424e7cae64872eb89f3eae
lauracristianas <45941279+lauracristianas@users.noreply.github.com>
2024-04-23 18:49:51 +0200

    created a solution and project

---
+++ PracticeSolution/PracticeProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PracticeProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); 
+        }
+    }
+}
